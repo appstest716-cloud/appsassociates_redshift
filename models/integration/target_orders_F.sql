a7741cb8e7a55db321e2e79d9ed24c220625a24d
Srinivas Guttula <srinivas.guttula@appsassociates.com>
2025-09-30 10:02:26 +0000

    asf

--- models/integration/target_orders_F.sql
+++ models/integration/target_orders_F.sql
@@ -1,4 +1,13 @@
 {{ config(materialized='table') }}
-select o.*, c.customer_id
+
+select
+  o.order_id,
+  o.customer_id,
+  o.order_ts,
+  o.order_total as amount,  -- or just keep `o.order_total`
+  c.first_name,
+  c.last_name,
+  c.email
 from {{ ref('orders_stg') }} o
-left join {{ ref('target_customers_D') }} c using (customer_id)
+left join {{ ref('target_customers_D') }} c
+  on c.customer_id = o.customer_id
